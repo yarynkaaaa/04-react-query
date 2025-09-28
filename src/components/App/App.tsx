159208300cd6d5fd331f39215307a82eb403553b
yarynkaaaa <usia98200@gmail.com>
2025-09-28 19:34:55 +0200

    delete 1 ryadochok

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,6 +7,7 @@ import MovieModal from "../MovieModal/MovieModal";
 import Loader from "../Loader/Loader";
 import toast, { Toaster } from "react-hot-toast";
 import { type Movie } from "../../types/movie";
+import { fetchMovies } from "../../services/movieService";
 import ReactPaginate from "react-paginate";
 import css from "./App.module.css";
 
